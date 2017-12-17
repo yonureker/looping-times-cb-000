a34554309c3fdf484ce2322a8451e43ceedebb49
Onur Eker <yonureker@gmail.com>
2017-12-17 07:33:47 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,7 +1,9 @@
 def using_times
-	#your code here
+	7.times do
+	  puts "Wingardium Leviosa"
+	end
 end
 
-
-
-
+# describe "#using_times" do
+# 	it "puts out the phrase 'Wingardium Leviosa' 7 times by using the `times` construct" do
+#
